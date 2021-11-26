11246b195d45befa702128cbcd4c836f158ec2d4
laialanza <laiaipatch@hotmail.com>
2021-11-26 21:10:14 +0100

    Ex 1

--- S2_1.py
+++ S2_1.py
@@ -1,5 +1,5 @@
 import subprocess
-video = input("Introduce the directory of the video you want to cut: \n ")
+video = input("Introduce the directory of the video you want to see motion vectors: \n ")
 
 subprocess.call(["ffmpeg", "-flags2", "export_mvs", "-i", video, "-vf",
                  "codecview=mv=pf+bf+bb", "motion_vectors.mp4"])
